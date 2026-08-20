a8be83c24dc2786672103f62235ec00b662961be
Jean Paiva <paivajean263@gmail.com>
2026-08-20 17:47:38 -0300

    ss

--- frontend/src/commponents/citizen/ComplaintForm.jsx
+++ frontend/src/commponents/citizen/ComplaintForm.jsx
@@ -39,6 +39,65 @@ const [errors, setErrors]= useState({});
                         {errors.dni && <p className="input-error">{errors.dni}</p>}
                         </span>
                     </label>
+
+                    <label>
+                        <span className="label-text">
+                        Nombre
+                        <span className="span-required">*</span>
+                        <input className="inputt"
+                        name="nombre"
+                        value={formData.nombre}
+                        />
+                        {errors.nombre && <p className="input-error">{errors.nombre}</p>}
+                        </span>
+                    </label>
+
+                    <label>
+                        <span className="label-text">
+                        Apellido
+                        <span className="span-required">*</span>
+                        <input className="inputt"
+                        name="apellido"
+                        value={formData.dni}
+                        />
+                        {errors.dni && <p className="input-error">{errors.dni}</p>}
+                        </span>
+                    </label>
+                    <label>
+                        <span className="label-text">
+                        Direccion
+                        <span className="span-required">*</span>
+                        <input className="inputt"
+                        name="direccion"
+                        value={formData.direccion}
+                        />
+                        {errors.direccion && <p className="input-error">{errors.direccion}</p>}
+                        </span>
+                    </label>
+
+                    <label>
+                        <span className="label-text">
+                        Numero de lote
+                        <span className="span-required">*</span>
+                        <input className="inputt"
+                        name="lote"
+                        value={formData.lote}
+                        />
+                        {errors.lote && <p className="input-error">{errors.lote}</p>}
+                        </span>
+                    </label>
+
+                    <label>
+                        <span className="label-text">
+                        Descripcion
+                        <span className="span-required">*</span>
+                        <textarea className="inputt"
+                        name=""
+                        value={formData.descripcion}
+                        />
+                        {errors.descripcion && <p className="input-error">{errors.descripcion}</p>}
+                        </span>
+                    </label>
                 </section>
 
                 <div className="complaint-actions">
