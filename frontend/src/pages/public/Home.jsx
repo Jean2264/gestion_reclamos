307842a43289288ad843907b35938165c6be3505
Jean Paiva <paivajean263@gmail.com>
2026-08-20 15:18:52 -0300

    Initial frontend setup

---
+++ frontend/src/pages/public/Home.jsx
@@ -0,0 +1,18 @@
+function Home(){
+    return(
+        <div className="home">
+            <h1>Gestion de reclamos    
+            </h1>
+
+            {/**Aca va a ir el header */}
+
+            <main className="home-container">
+
+            </main>
+
+            {/**Aca va a ir el footer */}
+        </div>
+    )
+}
+
+export default Home;
